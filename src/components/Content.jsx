510655eec7bbeff5b5dd34032735213644dfce9e
Rokibul Hasan Utso <mynetlify88@gmail.com>
2023-07-15 23:47:22 +0600

    react app

---
+++ src/components/Content.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+export default function Content() {
+  const [message, setMessage] = useState("");
+
+  const handleClick = () => {
+    // Get the text from the textarea
+    const text = document.getElementById("text").value;
+
+    // If the text is empty, show an error alert
+    if (text === "") {
+      alert("Please enter a message");
+    } else {
+      // Set the message in the p tag
+      setMessage(text);
+    }
+  };
+
+  return (
+    <main>
+      <div className="container">
+        <button className="btn" onClick={handleClick}>
+          Show Text
+        </button>
+        <textarea
+          name="text"
+          id="text"
+          placeholder="Write your text here...."
+        ></textarea>
+        <p id="showMessage">{message}</p>
+      </div>
+    </main>
+  );
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        ©<strong>AB SAGOR</strong>. All rights reserved.
+      </p>
+    </footer>
+  );
+}
